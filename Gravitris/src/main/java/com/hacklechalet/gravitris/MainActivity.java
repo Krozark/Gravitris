85fe90cbc50970d60dab6c55297534a54ae50b75
krozark <maxime.barbier1991@gmail.com>
2013-05-25 01:57:18 +0200

    add opengel render

--- Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
+++ Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
@@ -3,15 +3,20 @@ package com.hacklechalet.gravitris;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.opengl.GLSurfaceView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        setContentView(view);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
