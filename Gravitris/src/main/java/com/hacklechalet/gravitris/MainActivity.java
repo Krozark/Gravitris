a7b94276ff7b177af4be39754aee86c78f1dd607
Marc Schlatter <marc@moonscoop-fan.com>
2013-05-25 02:57:03 +0200

    added basic sensor

--- Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
+++ Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
@@ -1,5 +1,8 @@
 package com.hacklechalet.gravitris;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -7,12 +10,16 @@ import android.opengl.GLSurfaceView;
 
 public class MainActivity extends Activity {
 
+    protected SensorManager sensorManager;
+    protected Sensor sensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
         //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+        this.sensor = this.sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
         GLSurfaceView view = new GLSurfaceView(this);
         view.setRenderer(new OpenGLRenderer());
         setContentView(view);
