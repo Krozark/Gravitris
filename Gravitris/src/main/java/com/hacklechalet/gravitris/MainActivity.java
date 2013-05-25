d211eed8dd2cb1004dffc7183fe98c2e43bfa749
samuel <samuel.marpaux@sfr;fr>
2013-05-25 05:03:00 +0200

    adding little menu

--- Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
+++ Gravitris/src/main/java/com/hacklechalet/gravitris/MainActivity.java
@@ -1,33 +1,28 @@
 package com.hacklechalet.gravitris;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.opengl.GLSurfaceView;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-    protected SensorManager sensorManager;
-    protected Sensor sensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        //this.sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
-        //this.sensor = this.sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+
         Window win = this.getWindow();
         this.requestWindowFeature(win.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        GLSurfaceView view = new GLSurfaceView(this);
-        view.setRenderer(new OpenGLRenderer());
-        setContentView(view);
+
+        this.setContentView(R.layout.activity_main);
+
+        //this.startGame();
     }
 
     @Override
@@ -36,5 +31,12 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void startGame(View v)
+    {
+        GLSurfaceView view = new GLSurfaceView(this);
+        view.setRenderer(new OpenGLRenderer());
+        this.setContentView(view);
+    }
+
 }
