8d1bff9f0c666ea5f801bc4832bfd3c33a110869
samuel <samuel.marpaux@sfr;fr>
2013-05-26 12:52:35 +0200

    retour du score

--- Gravitris/src/main/java/com/hacklechalet/gravitris/GameActivity.java
+++ Gravitris/src/main/java/com/hacklechalet/gravitris/GameActivity.java
@@ -155,6 +155,7 @@ public class GameActivity extends Activity implements SensorEventListener {
             {
                 runOnUiThread(new Runnable() {
                     public void run() {
+                        textViewResScore.setText(String.valueOf(openGlRender.getScorePlayer()));
                         stringScore = String.valueOf(openGlRender.getScorePlayer());
 
                         //if(openGlRender.getStatusGame() == true)
